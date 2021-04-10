08ce4eb34de36b88e20cafc5745dd412ea7c73ab
Monika Manuela <manuela.monika97@gmail.com>
2021-04-10 13:02:27 +0800

    Update deps

--- setup.py
+++ setup.py
@@ -15,5 +15,16 @@ setuptools.setup(
     license='MIT',
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
+    python_requires='>=3.6.2',
+    install_requires=[
+        'language_tool_python',
+        'nltk',
+        'numpy',
+        'sentence_transformers',
+        'scipy==1.4.1',
+        'sentencepiece',
+        'torch',
+        'transformers',
+    ],
     zip_safe=False
 )
