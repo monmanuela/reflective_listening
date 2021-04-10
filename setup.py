7824f55b4eb6bc8ac1977c868a203c869a14e56a
Monika Manuela <manuela.monika97@gmail.com>
2021-04-10 13:04:10 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='reflective_listening',
-    version='0.1.2',
+    version='0.1.3',
     description='Reflective listening statements via paraphrase generation',
     long_description=long_description,
     long_description_content_type="text/markdown",
